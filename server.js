fc91479c3c8ca75161bdbd41112b987840da4252
akghosh111 <ghoshkiran2017@gmail.com>
2026-03-23 15:49:11 +0530

    add db config

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ import "dotenv/config"
 import app from "./src/app.js";
 
 
+const PORT = process.env.PORT|| 5000;
+
+
 const start = async() => {
     // connect to database
+    await connectDB();
 
     app.listen(PORT, () => {
         console.log(`Server is running at ${PORT} in ${process.env.NODE_ENV} mode`)
@@ -15,4 +19,4 @@ start().catch((err) => {
     console.error("Failed to start server", err);
     process.exit(1);
 })
-const PORT = process.env.PORT|| 5000;
+
