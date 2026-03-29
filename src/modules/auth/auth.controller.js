75ee758c4fe422233e3df985502a17cf991b43c1
akghosh111 <ghoshkiran2017@gmail.com>
2026-03-29 18:45:27 +0530

    add login/logout controller & route

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -7,7 +7,32 @@ const register = async() => {
     ApiResponse.created(res, "Registration success", user)
 }
 
+const login = async() => {
+    const {user, accessToken, refreshToken} =await authService.login(req.body)
+
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: true,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    res.cookie("accessToken", accessToken, {
+        httpOnly: true,
+        secure: true,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    ApiResponse.ok(res, "Login successful", {user, accessToken})
+
+}
+
+const logout = async() => {
+    await authService.logout(req.user.id);
+    res.clearCookie("refreshToken");
+    ApiResponse.ok(res, "Logout Success")
+}
 
 
 
-export {register}
+
+export {register, login, logout}
